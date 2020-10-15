33197557d6a56132af49afd1ee670afd80a3a870
y-u-y-a <yuya.ymd00@gmail.com>
2020-10-15 12:37:37 +0900

    変数修正

--- employee/src/app.js
+++ employee/src/app.js
@@ -1,17 +1,20 @@
 import { KintoneRecordManager } from './modules/index';
 
+const INDEX = 'app.record.index.show'; // 一覧ページ表示の際
+// const DETAIL = 'app.record.detail.show'; // レコード表示の際
+// const CREATE = 'app.record.create.show'; // 追加ページ表示の際
+// const EDIT = 'app.record.edit.show'; // レコード編集ページの際
+// const REPORT = 'app.report.show'; // グラフ表示
+
 (() => {
   "use strict";
-  // INDEX = 'app.record.index.show'; // 一覧ページ表示の際
-  // DETAIL = 'app.record.detail.show'; // レコード表示の際
-  // CREATE = 'app.record.create.show'; // 追加ページ表示の際
-  // EDIT = 'app.record.edit.show'; // レコード編集ページの際
-  // REPORT = 'app.report.show'; // グラフ表示
 
   // 一覧ページ
-  kintone.events.on('app.record.index.show', (event) => {
+
+  kintone.events.on(INDEX, (event) => {
     // 全てのレコード取得
-    (new KintoneRecordManager).getProcessingRecords()
+    let kintone_manager = new KintoneRecordManager;
+    let all_records = kintone_manager.getProcessingRecords()
     .then(response => {
       // レコード取得後の処理
       console.log(response.records);
